76c6ab963901731d0dfd5e2ddb6dbb4267e74e78
jordifierro <jordifierromulero@gmail.com>
2016-03-16 19:37:02 +0100

    Fix ./gradlew test, cAT and build

--- domain/src/test/java/com/jordifierro/androidbase/domain/entity/NoteEntityTest.java
+++ domain/src/test/java/com/jordifierro/androidbase/domain/entity/NoteEntityTest.java
@@ -3,8 +3,8 @@ package com.jordifierro.androidbase.domain.entity;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 public class NoteEntityTest {
 

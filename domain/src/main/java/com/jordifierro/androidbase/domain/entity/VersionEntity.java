3396e03214e127b344b84789fe626b0101d19532
jordifierro <jordifierromulero@gmail.com>
2016-04-22 17:33:06 +0200

    Refactor version entity

---
+++ domain/src/main/java/com/jordifierro/androidbase/domain/entity/VersionEntity.java
@@ -0,0 +1,18 @@
+package com.jordifierro.androidbase.domain.entity;
+
+public class VersionEntity {
+
+    private String expirationDate;
+
+    public VersionEntity(String expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public String getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(String expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+}
--- domain/src/test/java/com/jordifierro/androidbase/domain/entity/VersionEntityTest.java
+++ domain/src/test/java/com/jordifierro/androidbase/domain/entity/VersionEntityTest.java
@@ -6,27 +6,27 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
-public class MessageEntityTest {
+public class VersionEntityTest {
 
     private static final String FAKE_MSG = "fake message";
 
-    private MessageEntity message;
+    private VersionEntity message;
 
     @Before
     public void setup() {
-        this.message = new MessageEntity(FAKE_MSG);
+        this.message = new VersionEntity(FAKE_MSG);
     }
 
     @Test
     public void testMessageConstructor() {
-        assertThat(this.message.getMessage(), is(FAKE_MSG));
+        assertThat(this.message.getExpirationDate(), is(FAKE_MSG));
     }
 
     @Test
     public void testUserSetters() {
-        this.message.setMessage("another message");
+        this.message.setExpirationDate("another message");
 
-        assertThat(this.message.getMessage(), is("another message"));
+        assertThat(this.message.getExpirationDate(), is("another message"));
     }
 
 }

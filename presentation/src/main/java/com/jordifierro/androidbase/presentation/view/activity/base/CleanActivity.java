57e11552fc73ea34c8477632c36c02d33de469cc
jordifierro <jordifierromulero@gmail.com>
2016-04-22 21:33:18 +0200

    Implement upgrade redirect when version expired

--- presentation/src/main/java/com/jordifierro/androidbase/presentation/view/activity/base/CleanActivity.java
+++ presentation/src/main/java/com/jordifierro/androidbase/presentation/view/activity/base/CleanActivity.java
@@ -1,8 +1,11 @@
 package com.jordifierro.androidbase.presentation.view.activity.base;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.widget.Toast;
 
 import com.jordifierro.androidbase.data.net.error.RestApiErrorException;
@@ -35,9 +38,16 @@ public abstract class CleanActivity extends BaseActivity implements BaseView {
     @Override
     public void handleError(Throwable error) {
         if (error instanceof RestApiErrorException) {
-            if (((RestApiErrorException) error).getStatusCode()
-                    == RestApiErrorException.UNAUTHORIZED) closeAndDisplayLogin();
-            else showMessage(error.getMessage());
+            switch (((RestApiErrorException) error).getStatusCode()) {
+                case RestApiErrorException.UNAUTHORIZED:
+                    closeAndDisplayLogin();
+                    break;
+                case RestApiErrorException.UPGRADE_REQUIRED:
+                    createUpgradeDialog();
+                    break;
+                default:
+                    showMessage(error.getMessage());
+            }
         }
         else Toast.makeText(context(), getResources().getString(R.string.message_error),
                 Toast.LENGTH_LONG).show();
@@ -48,6 +58,24 @@ public abstract class CleanActivity extends BaseActivity implements BaseView {
         Intent notesIntent = new Intent(this, LoginActivity.class);
         notesIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(notesIntent);
+    }
+
+    private void createUpgradeDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(getResources().getString(R.string.message_expired) + ".\n" +
+                getResources().getString(R.string.message_update) + ".")
+                .setPositiveButton(R.string.message_ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        navigateToUpgrater();
+                    }
+                }).create().show();
+    }
+
+    private void navigateToUpgrater() {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("market://details?id=com.jordifierro.androidbase"));
+        startActivity(intent);
     }
 
     @Override
